fe6ed4fe67dcb6c6af4bafd9f286ebd3b0ce8f1a
pkyu <55111674+pkyu@users.noreply.github.com>
2023-03-21 00:51:40 -0400

    Daily Gamemode, More Stats, 2x Possible Coasters

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const popupOpenBtn = document.getElementById('[popup-open-target]');
 const popupCloseBtn = document.querySelectorAll('[popupCloseButton]');
 const popup = document.getElementById("popup");
 
+const today = Math.floor(Date.now() / 86400000);
+
+
+let gamemode = 'daily';
 var answer;
 var answerPark;
 var answerCountry;
 var currentRow;
-let paIDs = [2268,3056,3144,2832,2898,2206,716,2270,370,861,2229,2183,1783,2192,2135,2041,2111,2204,3068,178,2354,175,2233,187,2296,59,2137,2058,284,314,387,170,172,202];
+let paIDs = [917,2110,2268,3056,3144,2832,2898,2206,716,2270,370,861,2286,2229,2183,1783,2192,2135,2041,2111,2204,3068,178,2354,175,2233,187,2296,59,2137,2058,284,314,387,170,172,202,932,205,222,219,282,176,324,1635,201,289,2202,737,2478,1800,2335,2827,315,2849,2215,2028,270,3066,2946,2323,125];
 
 async function loadCoasters(query) {
     const URL = `https://vast-garden-04559.herokuapp.com/https://captaincoaster.com/api/coasters?page=1&name=${query}`;
@@ -65,6 +69,7 @@ function loadGuess() {
             })
             const guess = await result.json();
             console.log(guess);
+            if (gamemode == 'daily' && window.localStorage.getItem('gameEnd')) return;
             makeGuess(guess);
         });
     });
@@ -127,8 +132,13 @@ const state = {
 };
 
 async function startup() {
+    if (window.localStorage.getItem(`gamemode`)) gamemode = window.localStorage.getItem(`gamemode`);
+    if (gamemode == 'endless') checkMaybeSwitch();
+
     const game = document.getElementById('game');
-    let aID = paIDs[Math.floor(Math.random() * paIDs.length)];
+    var aID;
+    if (gamemode == 'endless') aID = paIDs[Math.floor(Math.random() * paIDs.length)];
+    else aID = getDailyCoaster();
     const URL = `https://vast-garden-04559.herokuapp.com/https://captaincoaster.com/api/coasters/${aID}`;
     const res = await fetch(`${URL}`, {
 			headers: {
@@ -148,6 +158,12 @@ async function startup() {
     answerCountry = answerCountry[0].toUpperCase() + answerCountry.substring(1);
     drawGrid(game);
     updateGrid();
+    if (gamemode == 'daily') updateDaily();
+    window.localStorage.setItem(`dayLastPlayed`, today);
+    if (window.localStorage.getItem(`daySwap`)) {
+        window.localStorage.removeItem('daySwap');
+        openPopup("gmReset");
+    }
 }
 
 startup();
@@ -254,15 +270,16 @@ function compareStats(guess, guessCountry) {
     document.getElementById(`tile${row}7`).classList.add('animated')
     document.getElementById(`tile${row}7`).style.animationDelay = `${(7 * animation_duration / 2)}ms`;
 
-    // Ending Game
     const isWinner = answer.id === guess.id;
     const isGameOver = (row === 5);
+    if (gamemode == 'daily' && window.localStorage.getItem('gameEnd')) return;
+    if (gamemode == 'daily' && row < 6) window.localStorage.setItem(`guess${(row+1)}`, guess.id);
     setTimeout(() => {
         if(isWinner) {
             //alert(`Congratulations! The answer was ${answer.name}!`);
             // Create confetti animation
             startConfetti();
-            addWin(row+1);
+            addWin(row+1,gamemode);
             document.getElementById('hmsg').innerHTML=(`You got it!`);
             document.getElementById('msg').innerHTML=(`The correct answer was ${answer.name} from ${answerPark.name}.`);
             openPopup("endgame");
@@ -270,12 +287,14 @@ function compareStats(guess, guessCountry) {
             setTimeout(function() {
                 stopConfetti();
             }, 3000);
+            window.localStorage.setItem('gameEnd','true');
         }
         else if (isGameOver) {
             document.getElementById('hmsg').innerHTML=(`Out of turns :((`);
             document.getElementById('msg').innerHTML=(`The correct answer was ${answer.name} from ${answerPark.name}.`);
-            addLoss();
+            addLoss(gamemode);
             openPopup("endgame");
+            window.localStorage.setItem('gameEnd','true');
         }
         else if (row === 2) {
             document.getElementById(`tile28`).innerHTML = `
@@ -297,27 +316,74 @@ function compareStats(guess, guessCountry) {
         }
     }, 4.5 * animation_duration);
 }
-
 function openPopup(name) {
     document.getElementById(name).classList.add('open');
     overlay.classList.add('active');
 }
+
 function closePopup(name) {
     document.getElementById(name).classList.remove('open');
     overlay.classList.remove('active');
 }
 
 
+// Daily?
+function getDailyCoaster() {
+    return paIDs[Math.floor(Date.now() / 86400000)%paIDs.length];
+}
+function checkMaybeSwitch() {
+    const dayLastPlayed = window.localStorage.getItem(`dayLastPlayed`);
+    window.localStorage.setItem(`dayLastPlayed`, today)
+    console.log(today);
+    if (dayLastPlayed != today) {
+        for (let i = 1; i < 7; i++) {
+            window.localStorage.removeItem(`guess${i}`,);
+        }
+        window.localStorage.removeItem(`gameEnd`);
+        window.localStorage.setItem(`daySwap`,'true');
+        switchGamemode();
+    }
+}
+async function updateDaily() {
+    const dayLastPlayed = window.localStorage.getItem(`dayLastPlayed`);
+    window.localStorage.setItem(`dayLastPlayed`, today)
+    if (dayLastPlayed != today) {
+        for (let i = 1; i < 7; i++) {
+            window.localStorage.removeItem(`guess${i}`,);
+        }
+        window.localStorage.removeItem(`gameEnd`);
+    }
+    else {
+        const timer = ms => new Promise(res => setTimeout(res, ms));
+        for (let i = 1; i < 7; i++) {
+            const currGuess = window.localStorage.getItem(`guess${i}`);
+            if (currGuess) {
+                let guessID = parseInt(currGuess);
+                const result = await fetch(`https://vast-garden-04559.herokuapp.com/https://captaincoaster.com/api/coasters/${guessID}`, {
+                    headers: {
+                        'X-AUTH-TOKEN':'b154f4d4-bbda-404b-ae3e-ea959f68120d'
+                    }
+                })
+                const guess = await result.json();
+                console.log(guess);
+                makeGuess(guess);
+                await timer(50);
+            }
+            else break;
+        }
+    }
+}
 // Local Storage Stats Page (yippe)
-// I'm a Winner
-const canvasElement = document.getElementById("statChart");
-const config = {
+const canvasElementDaily = document.getElementById("dailyStatChart");
+const canvasElementEndless = document.getElementById("endlessStatChart");
+
+const configDaily = {
     type: "bar",
     data: {
         labels: ["1","2","3","4","5","6"],
         datasets: [{
             label: "Games Won",
-            data: loadStats(),
+            data: loadStats('daily'),
             backgroundColor: [
                 "rgba(255, 99, 132, 0.3)",
                 "rgba(255, 159, 64, 0.3)",
@@ -362,16 +428,14 @@ const config = {
     }
     
 };
-var statChart = new Chart(canvasElement, config); 
-function addWin(turn) {
-    const rowScore = window.localStorage.getItem(`row${turn}score`);
-    if (!rowScore) window.localStorage.setItem(`row${turn}score`, 1);
-    else localStorage.setItem(`row${turn}score`,parseInt(localStorage.getItem(`row${turn}score`))+1);
-    statChart.config.data = {
+
+const configEndless = {
+    type: "bar",
+    data: {
         labels: ["1","2","3","4","5","6"],
         datasets: [{
             label: "Games Won",
-            data: loadStats(),
+            data: loadStats('endless'),
             backgroundColor: [
                 "rgba(255, 99, 132, 0.3)",
                 "rgba(255, 159, 64, 0.3)",
@@ -390,20 +454,124 @@ function addWin(turn) {
             ],
             borderWidth: 1,
         }],
-    };
-    statChart.update();
+    },
+    options: {
+        indexAxis: 'y',
+        scales: {
+            x: {
+                suggestedMax: 5,
+                title: {
+                    display: true,
+                    text: 'Games'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Turns'
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+    
+};
+
+
+var dailyStatChart = new Chart(canvasElementDaily, configDaily); 
+var endlessStatChart = new Chart(canvasElementEndless, configEndless);
+
+function addWin(turn,gamemode) {
+    const wins = window.localStorage.getItem(`wins${gamemode}`);
+    if (!wins) window.localStorage.setItem(`wins${gamemode}`, 1);
+    else localStorage.setItem(`wins${gamemode}`,parseInt(localStorage.getItem(`wins${gamemode}`))+1);
+    const rowScore = window.localStorage.getItem(`row${turn}score${gamemode}`);
+    if (!rowScore) window.localStorage.setItem(`row${turn}score${gamemode}`, 1);
+    else localStorage.setItem(`row${turn}score${gamemode}`,parseInt(localStorage.getItem(`row${turn}score${gamemode}`))+1);
+    if (gamemode == 'daily')  {
+        dailyStatChart.config.data = {
+            labels: ["1","2","3","4","5","6"],
+            datasets: [{
+                label: "Games Won",
+                data: loadStats(gamemode),
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.3)",
+                    "rgba(255, 159, 64, 0.3)",
+                    "rgba(240, 230, 140, 0.5)",
+                    "rgba(75, 192, 192, 0.3)",
+                    "rgba(54, 162, 235, 0.3)",
+                    "rgba(153, 102, 255, 0.3)"
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132 , 1)",
+                    "rgba(255, 159, 64, 1)",
+                    "rgba(240, 230, 100, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(153, 102, 255, 1)"
+                ],
+                borderWidth: 1,
+            }],
+        };
+        dailyStatChart.update();
+    }
+    else {
+        endlessStatChart.config.data = {
+            labels: ["1","2","3","4","5","6"],
+            datasets: [{
+                label: "Games Won",
+                data: loadStats(gamemode),
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.3)",
+                    "rgba(255, 159, 64, 0.3)",
+                    "rgba(240, 230, 140, 0.5)",
+                    "rgba(75, 192, 192, 0.3)",
+                    "rgba(54, 162, 235, 0.3)",
+                    "rgba(153, 102, 255, 0.3)"
+                ],
+                borderColor: [
+                    "rgba(255, 99, 132 , 1)",
+                    "rgba(255, 159, 64, 1)",
+                    "rgba(240, 230, 100, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(153, 102, 255, 1)"
+                ],
+                borderWidth: 1,
+            }],
+        };
+        endlessStatChart.update();
+    }
 }
-function addLoss() {
-    const losses = window.localStorage.getItem(`losses`);
-    if (!losses) window.localStorage.setItem(`losses`, 1);
-    else localStorage.setItem(`losses`,parseInt(localStorage.getItem('losses'))+1);
+function addLoss(gamemode) {
+    const losses = window.localStorage.getItem(`losses${gamemode}`);
+    if (!losses) window.localStorage.setItem(`losses${gamemode}`, 1);
+    else localStorage.setItem(`losses${gamemode}`,parseInt(localStorage.getItem(`losses${gamemode}`))+1);
 }
-function loadStats() {
+function loadStats(gamemode) {
     const scores = []
     for (let i = 1; i < 7; i++) {
-        const rowScore = window.localStorage.getItem(`row${i}score`);
+        const rowScore = window.localStorage.getItem(`row${i}score${gamemode}`);
         if (!rowScore) scores.push(0);
-        else scores.push(parseInt(localStorage.getItem(`row${i}score`)));
+        else scores.push(parseInt(localStorage.getItem(`row${i}score${gamemode}`)));
     }
     return scores;
+}
+
+if (gamemode == 'endless') {
+    document.getElementById("gmButton").src="assets/endless.png";
+}
+
+function switchGamemode() {
+    if (gamemode == 'daily') {
+        window.localStorage.setItem('gamemode','endless');
+    }
+    else {
+        window.localStorage.setItem('gamemode','daily');
+    }
+    location.reload();
 }
